aa52a76e4e24bf0cc7d15e8de6715a906117c565
mail-aizhan <65739091+mail-aizhan@users.noreply.github.com>
2021-04-04 21:49:48 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,67 @@
+var user = prompt("Adiniz nedir?")
+let visitor = document.getElementById('myName')
+
+
+
+if (user !=null){
+    visitor.innerHTML = ` ${user} `}
+
+
+     setInterval(showTime, 500);
+    function showTime() {
+        var time = new Date();
+        var hrs = time.getHours();
+        var min = time.getMinutes();
+        var sec =time.getSeconds();
+        var day =time.getDay();
+       
+        
+            switch(day){
+              case 0:
+                day="Sunday"
+                break;
+                case 1:
+                  day="Monday"
+                  break;
+                  case 2:
+                    day="Tuesday"
+                    break;
+                    case 3:
+                      day="Wednesday"
+                      break;
+                      case 4:
+                        day="Thursday"
+                        break;
+                        case 5:
+                          day="Friday"
+                          break;
+                          case 6:
+                            day="Saturday"
+                            break; 
+            }
+
+        let en = "AM";
+
+        if(hrs > 12){
+          en ="PM";
+        }
+
+
+        if(hrs>12){
+          hrs = hrs - 12;
+        }
+        if (hrs == 0){
+          hrs = 12;
+        }
+        if(min < 10){
+          min ="0" + min;
+        }
+        if(sec< 10){
+          sec ="0" + sec;
+        }
+        
+
+
+        document.getElementById('myClock').innerHTML = `${hrs}: ${min}: ${sec} ${en} ${day} `
+      }
+      window.onload = showTime;
